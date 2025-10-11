cfdaed55e8e0deae95def426f8f7be25bc916941
Keensu <yabizunda@gmail.com>
2025-10-11 18:57:45 +0300

    Add files via upload
    Так как доделывал SIngleton, а предыдущая версия не сохранилась, здесь присутствует классная работа со следующей пары(кусок HW_4)

---
+++ Homework_3/MiniHttpServer/Core/Handlers/EndPointsHandler.cs
@@ -0,0 +1,54 @@
+﻿using MiniHttpServer.Core.Abstracts;
+using MiniHttpServer.Core.Attributes;
+using MiniHttpServer.shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Core.Handlers
+{
+    class EndPointsHandler : Handler
+    {
+        public override void HandleRequest(HttpListenerContext context)
+        {
+            // некоторая обработка запроса
+
+            if (true)
+            {
+                var request = context.Request;
+                var endpointName = request.Url?.AbsolutePath.Split('/')
+                    .Where(p => p != string.Empty || p != null).First();
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var endpoint = assembly.GetTypes()
+                    .Where(t => t.GetCustomAttribute<Endpoint>() != null)
+                    .FirstOrDefault(end => IsCheckedNameEndpoint(end.Name, endpointName));
+
+                if (endpoint == null) return; //hw
+
+                var method = endpoint.GetMethods().Where(t => t.GetCustomAttributes(true)
+                    .Any(attr => attr.GetType().Name.Equals($"Http{context.Request.HttpMethod}", StringComparison.OrdinalIgnoreCase))).FirstOrDefault();
+
+                if (method == null) return; //hw
+
+                var ret = method.Invoke(Activator.CreateInstance(endpoint), null);
+
+            }
+
+            // передача запроса дальше по цепи при наличии в ней обработчиков
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(context);
+            }
+        }
+
+        private bool IsCheckedNameEndpoint(string endpointName, string className) =>
+            endpointName.Equals(className, StringComparison.OrdinalIgnoreCase) ||
+            endpointName.Equals($"{className}Endpoint", StringComparison.OrdinalIgnoreCase);
+   }
+}
---
+++ Homework_3/MiniHttpServer/EndPoints/AuthEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using MiniHttpServer.Core.Attributes;
+using MiniHttpServer.Services;
+
+namespace MiniHttpServer.EndPoints
+{
+    [Endpoint]
+    internal class AuthEndpoint
+    {
+        //get /auth/
+        [HttpGet]
+        public string LoginPage()
+        {
+            return "index.html";
+        }
+        //post /auth/
+        [HttpPost]
+        public void Login(string email, string password) {
+            //send email and password
+            //EmailService.SendEmail(email, title, message);
+        }
+
+        // post /auth/sendEmail
+        [HttpPost("sendEmail")]
+        public void SendEmail(string to, string title, string message)
+        {
+            
+        }
+    }
+}
