d64d41dc8efec9826297cbd743598bd772e5ca78
Keensu <yabizunda@gmail.com>
2025-10-29 22:28:56 +0300

    Add files via upload
    Homework_4

---
+++ MiniHttpServer/MiniHttpServer.Framework/Core/HttpResponse/PageResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Framework.Core.HttpResponse
+{
+    public class PageResult : IHttpResult
+    {
+        private readonly string _pathTemplate;
+        private readonly object _data;
+        public PageResult(string pathTemplate, object data)
+        {
+            _pathTemplate = pathTemplate;
+            _data = data;
+        }
+        public string Execute(HttpListenerContext context) 
+        {
+            //Todo
+            // вызов с шаблонизатора
+            // Json result
+            // tests framework.unittetst
+            // tests http server
+            // logika endpointhandler
+            return String.Empty;
+        }
+    }
+}
---
+++ MiniHttpServer/MiniTemplateAgile/IHtmlTemplateRenderer.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniTemplateAgile
+{
+    public interface IHtmlTemplateRenderer
+
+    {
+        string RenderFromString(string htmlTemplate, object dataModel);
+
+        string RenderFromFile(string filePath, object dataModel);
+
+        string RenderToFile(string inputFilePath, string outputFilePath, object dataModel);
+
+    }
+}
