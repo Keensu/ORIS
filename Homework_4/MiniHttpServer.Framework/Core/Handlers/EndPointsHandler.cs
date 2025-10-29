adf99853dabc30e94b02b34074ab51b199e7384a
Keensu <yabizunda@gmail.com>
2025-10-29 22:30:01 +0300

    Add files via upload

---
+++ Homework_4/MiniHttpServer.Framework/Core/Handlers/EndPointsHandler.cs
@@ -0,0 +1,102 @@
+﻿using MiniHttpServer.Core.Abstracts;
+using MiniHttpServer.Core.Attributes;
+using MiniHttpServer.Framework.Core;
+using MiniHttpServer.shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Core.Handlers
+{
+    class EndPointsHandler : Handler
+    {
+        public override async void HandleRequest(HttpListenerContext context)
+        {
+
+            if (true)
+            {
+                var request = context.Request;
+                var endpointName = request.Url?.AbsolutePath.Split('/')
+                    .Where(p => p != string.Empty && p != null).First();
+
+                var assembly = Assembly.GetEntryAssembly();
+                var endpoint = assembly.GetTypes()
+                    .Where(t => t.GetCustomAttribute<Endpoint>() != null)
+                    .FirstOrDefault(end => IsCheckedNameEndpoint(end.Name, endpointName));
+
+                if (endpoint == null) return; //hw
+
+                var method = endpoint.GetMethods().Where(t => t.GetCustomAttributes(true)
+                    .Any(attr => attr.GetType().Name.Equals($"Http{context.Request.HttpMethod}", StringComparison.OrdinalIgnoreCase))).FirstOrDefault();
+
+                if (method == null) return; //hw
+
+                var reader = new StreamReader(request.InputStream, request.ContentEncoding);
+                var body = reader.ReadToEnd();
+
+                var postParams = new Dictionary<string, string>();
+                foreach (var pair in body.Split('&'))
+                {
+                    var kv = pair.Split('=');
+                    postParams[WebUtility.UrlDecode(kv[0])] = WebUtility.UrlDecode(kv[1]);
+                }
+
+                var parameters = method.GetParameters()
+                                         .Select(p => postParams.ContainsKey(p.Name) ? postParams[p.Name] : null)
+                                         .ToArray();
+
+                bool isBaseEndpoint = endpoint.Assembly.GetTypes()
+                                      .Any(t => typeof(EndpointBase)
+                                      .IsAssignableFrom(t) && !t.IsAbstract);
+
+                var instanceEndpoint = Activator.CreateInstance(endpoint);
+
+                if (isBaseEndpoint)
+                {
+                    (instanceEndpoint as EndpointBase).SetContext(context);
+                }
+
+                var result = method.Invoke(Activator.CreateInstance(endpoint), parameters);
+
+                if(result is string stringContent)
+                {
+                    await WriteResponseAsync(context.Response, stringContent);
+                }
+
+                else
+                {
+                    //hw
+                    await WriteResponseAsync(context.Response, result.ToString() ?? "Sending data. Status(OK)");
+                }
+            }
+
+            // передача запроса дальше по цепи при наличии в ней обработчиков
+
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(context);
+            }
+        }
+
+        private bool IsCheckedNameEndpoint(string endpointName, string className) =>
+            endpointName.Equals(className, StringComparison.OrdinalIgnoreCase) ||
+            endpointName.Equals($"{className}Endpoint", StringComparison.OrdinalIgnoreCase);
+
+        private static async Task WriteResponseAsync(HttpListenerResponse response, string content)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(content);
+            // получаем поток ответа и пишем в него ответ
+            response.ContentLength64 = buffer.Length;
+            using Stream output = response.OutputStream;
+            // отправляем данные
+            await output.WriteAsync(buffer);
+            await output.FlushAsync();
+
+        }
+   }
+}
---
+++ Homework_4/MiniHttpServer.Framework/shared/HttpServer.cs
@@ -0,0 +1,74 @@
+﻿using MiniHttpServer.Core.Abstracts;
+using MiniHttpServer.Core.Handlers;
+using MiniHttpServer.Settings;
+using System.Net;
+using System.Net.Mime;
+
+namespace MiniHttpServer.shared
+{
+    public class HttpServer
+    {
+
+        private HttpListener _listener = new();
+        private SettingsModel _config;
+        private bool _isRunning = false;
+
+        public HttpServer(SettingsModel config) {
+            _config = config;
+            Config.Initialize(config);
+        }
+
+        public void Start()
+        {
+
+            _listener.Prefixes.Add($"http://{_config.Domain}:{_config.Port}/");
+            _listener.Start();
+            _isRunning = true;
+            Console.WriteLine("Сервер запущен");
+            Receive();
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+            _listener.Stop();
+            Console.WriteLine("Сервер остановил работу");
+        }
+
+        private void Receive()
+        {
+            if (_isRunning)
+            {
+                try
+                {
+                    _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+                }
+
+                catch (Exception ex) 
+                { 
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else {return; }
+        }
+
+        private async void ListenerCallback(IAsyncResult result)
+        {
+            if (_listener.IsListening)
+            {
+                if(!_isRunning) {return; }
+
+                var context = _listener.EndGetContext(result);
+
+                Handler staticFilesHandler = new StaticFilesHandler();
+                Handler endPointsHandler = new EndPointsHandler();
+                staticFilesHandler.Successor = endPointsHandler;
+                staticFilesHandler.HandleRequest(context);
+
+
+                Console.WriteLine("Запрос обработан");
+                Receive();
+            }
+        }
+    }
+}
